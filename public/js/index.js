9686b63a33463810464df2c339bd1eb41c8a9cb8
impact graphics <impactgraphics98@gmail.com>
2022-06-21 01:39:20 -0700

    init commit

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+console.log('hi, what is good')
+
+
+booknow = () => {
+    const book = document.getElementById('booking')
+    book.classList.remove('visibility')
+}
---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/service', (req, res) => {
+    res.render('service')
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('server is up, continue comerade')
+})
