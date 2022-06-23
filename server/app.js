f41e1f7062f3666460b4817fef56eb13c3c34c1e
impact graphics <impactgraphics98@gmail.com>
2022-06-23 13:03:25 -0700

    hh

--- server/app.js
+++ server/app.js
@@ -5,6 +5,14 @@ const ejs = require('ejs')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] !== 'https')
+            return res.redirect('https://' + req.headers.host + req.url);
+    } else
+        return next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
