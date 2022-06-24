552cedc9e0abf9a4bc6f37c9d8830d6066a6ccd4
impact graphics <impactgraphics98@gmail.com>
2022-06-24 04:26:50 -0700

    ssl

--- server/app.js
+++ server/app.js
@@ -5,14 +5,14 @@ const ejs = require('ejs')
 const app = express()
 const port = process.env.PORT || 3000
 
-if(process.env.NODE_ENV === 'production') {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') !== 'https')
-        res.redirect(`https://${req.header('host')}${req.url}`)
-      else
-        next()
-    })
-  }
+// if(process.env.NODE_ENV === 'production') {
+//     app.use((req, res, next) => {
+//       if (req.header('x-forwarded-proto') !== 'https')
+//         res.redirect(`https://${req.header('host')}${req.url}`)
+//       else
+//         next()
+//     })
+//   }
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
